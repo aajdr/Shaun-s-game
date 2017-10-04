7db033c86d75166adce763f48356a631c4f7c4e1
aajdr <aarondudedrake@gmail.com>
2017-10-04 17:14:55 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,5 +3,16 @@
 int main()
   {
   
+  int turn = 1;
+  bool possible = false;
+  bool army1 = true;
+  bool army2 = false;
+  bool army3 = true;
+  bool army4 = false;
+  
+  //graphics/sprites go here
+  
+  
+  
   }
   
